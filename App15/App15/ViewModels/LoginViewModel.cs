80bc63339b0cc4a87ea24b46e3f175c14613d54c
asierdanjou <asierdanjou@gmail.com>
2018-09-12 13:25:52 +0200

    SinBaseViewModel

--- App15/App15/ViewModels/LoginViewModel.cs
+++ App15/App15/ViewModels/LoginViewModel.cs
@@ -5,26 +5,146 @@ using System.Text;
 
 namespace App15.ViewModels
 {
+    using GalaSoft.MvvmLight.Command;
+    using System.ComponentModel;
     using System.Windows.Input;
+    using Xamarin.Forms;
 
-    class LoginViewModel
+    // Clase con interface INotifyPropertyChanged
+
+    class LoginViewModel : INotifyPropertyChanged
     {
-        #region Properties
-        public string Email { get; set; }
-        public string Password { get; set; }
-        public bool IsRunning { get; set; }
-        public bool IsRemembered { get; set; }
+        #region Events
+        public event PropertyChangedEventHandler PropertyChanged;
         #endregion
 
+        // Atributos privados de la clase
+        // En este ejemplo creamos las que queremos refrecar en tiempo de ejecucion.
+        #region Attributes      
+        private string password;    // Atributo privado en minuscula
+        private bool isRunning;
+        private bool isEnabled;
+        #endregion
+
+        // Propiedades publicas de la clase (bindadas a los elementos de la LoginPage)
+        #region Properties
+        public string Email { get; set; }
+        public string Password
+        {
+            get
+            {
+                return this.password;
+            }
+            set
+            {
+                if (this.password != value)
+                {
+                    this.password = value;
+                    PropertyChanged?.Invoke(
+                        this,
+                        new PropertyChangedEventArgs(nameof(this.Password))
+                    );
+                }
+            }
+        }
+        public bool IsRunning
+        {
+            get
+            {
+                return this.isRunning;
+            }
+            set
+            {
+                if (this.isRunning != value)
+                {
+                    this.isRunning = value;
+                    PropertyChanged?.Invoke(
+                        this,
+                        new PropertyChangedEventArgs(nameof(this.IsRunning))
+                    );
+                }
+            }
+        }
+        public bool IsRemembered { get; set; }
+        public bool IsEnabled
+        {
+            get
+            {
+                return this.isEnabled;
+            }
+            set
+            {
+                if (this.isEnabled != value)
+                {
+                    this.isEnabled = value;
+                    PropertyChanged?.Invoke(
+                        this,
+                        new PropertyChangedEventArgs(nameof(this.IsEnabled))
+                    );
+                }
+            }
+        }
+        #endregion
+
+        // Constructor de elementos. Por defecto son False.
         #region Constructors
         public LoginViewModel()
         {
             this.IsRemembered = true;
+            this.IsEnabled = true;
         }
         #endregion
 
+        // Commandos de la clase. Acciones bindadas.
         #region Commands
-        public ICommand LoginCommand { get; set; }
+        public ICommand LoginCommand
+        {
+            get
+            {
+                return new RelayCommand(Login);
+            }
+        }
+
+        private async void Login()
+        {
+            if (string.IsNullOrEmpty(this.Email))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter an email.",
+                    "Accept");
+                return;
+            }
+            if (string.IsNullOrEmpty(this.Password))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter an password.",
+                    "Accept");
+                return;
+            }
+            this.IsRunning = true;
+            this.IsEnabled = false;
+            if (this.Email != "asierdanjou@gmail.com" || this.Password != "1234")
+            {
+                this.IsRunning = false;
+                this.IsEnabled = true;
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "Email or password incorrect.",
+                    "Accept");
+                this.Password = string.Empty;
+                return;
+            }
+            this.IsRunning = false;
+            this.IsEnabled = true;
+
+            await Application.Current.MainPage.DisplayAlert(
+                "Ok",
+                "CORRECT",
+                "Accept");
+
+        }
         #endregion
     }
 }
